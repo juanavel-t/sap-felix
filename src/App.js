37bd3530a573b216aa66dffb4f425eb12455e108
juanavel-t <juanavelasquez2603@gmail.com>
2020-08-05 09:28:52 -0500

    equipo 2 modifiqué tabla en js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tabla de integrantes
+      </h1>
+      div
+    
+    </div>
+  );
+}
+
+export default App;
